bc60a149e3239d50e904911107c437748e3c99de
manh-td <dmtran120901@gmail.com>
2026-06-25 17:07:21 +1000

    Update

--- bash-scripts/clean-results.sh
+++ bash-scripts/clean-results.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rsync -avm --include='*/' --include='RunnerResult_DefaultRefiner.json' --exclude='*' output/ jit.pocgen.gpt-4o-mini/
-rsync -avm --include='*/' --include='prompt.json' --exclude='*' output/ jit.pocgen.gpt-4o-mini/
+rsync -avm --include='*/' --include='RunnerResult_DefaultRefiner.json' --exclude='*' $1 $2
+rsync -avm --include='*/' --include='prompt.json' --exclude='*' $1 $2
